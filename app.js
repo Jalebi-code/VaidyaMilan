3ce059ddca14ceb1883945291284f1224e7a529f
Kameshaditya60 <66486121+Kameshaditya60@users.noreply.github.com>
2024-09-12 08:07:32 +0530

    updated app.js and package.json

--- app.js
+++ app.js
@@ -5,27 +5,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 const authRoutes = require('./routes/authRoutes');
-const Doctor = require('./models/Doctors');
-const generateDoctors = require('./dummyDrGenerator/generateRandomDoctor');
-
-
-//generate random doctors data
-const insertDoctors = async () => {
-    mongoose.connect(process.env.MONGO_URL)
-    const doctors = generateDoctors(10); // Example: Generate 10 doctors
-    console.log('Generated doctors:', doctors);
-    mongoose.connection.close();
-    try {
-        const result = await Doctor.insertMany(doctors);
-        console.log('Doctors inserted successfully', result);
-    } catch (err) {
-        console.error('Error inserting doctors', err);
-    }
-    
-};
-
-// Run the insertion to generate and insert ducuments(doctors list data) VaidyaMilan Collection in Database 
-// insertDoctors();  // commented this out because already inserted doctors data in DB successfully
 
 dotenv.config();
 app.use(express.json());
